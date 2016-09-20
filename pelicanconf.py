b80872c679be5976b64f27502468ee12f53b4f3c
Ivan Habunek <ivan.habunek@gmail.com>
2016-09-20 16:33:28 +0200

    Set site URL

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Ivan Habunek'
 SITENAME = 'Ivan Habunek'
-# SITEURL = 'http://ivan.habunek.com/'
+SITEURL = 'https://ivan.habunek.com'
 
 PATH = 'content'
 
